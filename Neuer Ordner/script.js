6a289a0046cc4c7d95f78e8fa94735cf82af675b
Lucian-Cristian Toma <vamix2012@gmail.com>
2020-11-06 15:45:12 +0100

    when fields are clicked a shape will be drawn

--- Neuer Ordner/script.js
+++ Neuer Ordner/script.js
@@ -8,13 +8,15 @@ function fillShape(id) {
     } else {
         currentShape = 'cross';
     }
+
     fields[id] = currentShape;
     console.log(fields);
     draw();
 }
+
 function draw() {
-    for (let i = 0; i< fields.length; i++){
-         if (fields[i] == 'circle') {
+    for (let i = 0; i < fields.length; i++) {
+        if (fields[i] == 'circle') {
             document.getElementById('circle-' + i).classList.remove('d-none');
         }
 
@@ -22,5 +24,4 @@ function draw() {
             document.getElementById('cross-' + i).classList.remove('d-none');
         }
     }
-   
 }
